4a113b1a511205d8fd593941a541aaa30a31952f
egillmagnus <124277422+egillmagnus@users.noreply.github.com>
2023-03-17 09:04:11 +0000

    Egill second commit

---
+++ src/main/java/is/hi/hbv202g/ass9/compositeLeafObservedByComposite/MultiplyComposite.java
@@ -0,0 +1,31 @@
+package is.hi.hbv202g.ass9.compositeLeafObservedByComposite;
+
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MultiplyComposite implements MathExpression {
+    private List<MathExpression> children = new ArrayList<MathExpression>();
+
+    public void add(MathExpression child) {
+        children.add(child);
+    }
+
+    public void remove(MathExpression child) {
+        children.remove(child);
+    }
+
+    public List<MathExpression> getChildren() {
+        return children;
+    }
+
+    public int getResult() {
+        int result = 1;
+        for (MathExpression child : children) {
+            result *= child.getResult();
+        }
+        return result;
+    }
+
+
+}
