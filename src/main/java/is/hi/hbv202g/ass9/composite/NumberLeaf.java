23f73dd6bca412c8f10a4c4a8bceec1632ec0f24
egillmagnus <124277422+egillmagnus@users.noreply.github.com>
2023-03-17 08:45:17 +0000

    Egill first commit

---
+++ src/main/java/is/hi/hbv202g/ass9/composite/NumberLeaf.java
@@ -0,0 +1,12 @@
+package is.hi.hbv202g.ass9.composite;
+
+public class NumberLeaf {
+    private int value;
+    public NumberLeaf(int value) {
+        this.value = value;
+    }
+
+    public int getValue() {
+        return value;
+    }
+}
